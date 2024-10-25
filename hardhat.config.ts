39088eaac5c35d5b7d1ef8de0c684d5c97ae7c5e
Mitchell Douglass <mrdouglass95@gmail.com>
2024-10-25 01:56:14 +0000

    try this

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,7 +11,7 @@ const { vars } = require("hardhat/config");
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and add it to the configuration variables
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY", "");
 
 // Add your Sepolia account private key to the configuration variables
 // To export your private key from Coinbase Wallet, go to
@@ -19,7 +19,7 @@ const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY", "");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
